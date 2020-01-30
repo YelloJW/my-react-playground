8c1efc00d51b9d3e7e9c4eb47e97f3a03f6bec73
Joe Olley <joeolley@hotmail.co.uk>
2020-01-30 14:39:23 +0000

    weather card component added with navigator geolocation and open weather api calls. Weather card rendered in app component

--- geolocation-and-weather/src/index.js
+++ geolocation-and-weather/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WeatherCard from './WeatherCard'
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello world</div>
+      <WeatherCard/>
     )
   }
 }
