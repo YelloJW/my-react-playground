3bb833f732578d939232a65e12a9b80b087d037a
Joe Olley <joeolley@hotmail.co.uk>
2020-01-30 16:51:00 +0000

    geolocation methods moved to index.js file and lat and lon passed as props to child components

--- geolocation-and-weather/src/index.js
+++ geolocation-and-weather/src/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WeatherCard from './WeatherCard'
+import WeatherCard from './weather_card'
+import WeatherForecast from './weather_forecast'
+
+import './index.css'
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lat: null,
+      lon: null,
+      errorMessage: "",
+    }
+  }
+
+  getCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    )
+  }
+
+  componentDidMount() {
+    this.getCoordinates()
+  }
+
   render() {
     return (
-      <WeatherCard/>
+      <div>
+        <WeatherCard lat={this.state.lat} lon={this.state.lon}/>
+        <WeatherForecast lat={this.state.lat} lon={this.state.lon}/>
+      </div>
     )
   }
 }
