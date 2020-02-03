6b54552b974b2a7e8f76f76fbb7f28205764886c
Joe Olley <joeolley@hotmail.co.uk>
2020-02-03 17:29:33 +0000

    conditional formatting of forecast cards to show alternating days

--- geolocation-and-weather/src/components/WeatherForecastCard.js
+++ geolocation-and-weather/src/components/WeatherForecastCard.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 class ForecastCard extends React.Component {
-  configureTime() {
-
+  formatDay(dateTime) {
+    return dateTime.getDay() % 2 === 0 ? "light-background": "dark-background";
   }
 
   render(){
     const options = { weekday: 'long', hour:'numeric'};
-    const dateTime = new Date(this.props.dateTime * 1000).toLocaleString('en-US', options)
+    const dateTime = new Date(this.props.dateTime * 1000)
+    const dateTimeString = dateTime.toLocaleString('en-US', options)
     return (
       <div className="forecast-grid-card">
-        <h1 className="forecast-grid-card-datetime">{dateTime}</h1>
+        <h1 className={`forecast-grid-card-datetime ${this.formatDay(dateTime)}`}>{dateTimeString}</h1>
         <div className="forecast-grid-card-content">
           <div>
             <img src={`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`} alt="" />
