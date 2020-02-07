b4da13cf42e9891bc9a9d40af4c5053b7ac6133e
Joe Olley <joeolley@hotmail.co.uk>
2020-02-07 10:52:16 +0000

    activity component added with select activity callback

--- strava-integration/src/components/Activity.jsx
+++ strava-integration/src/components/Activity.jsx
@@ -1,1 +1,22 @@
-Activity.jsx;
+import React from "react";
+
+const Activity = props => {
+  return (
+    <tr
+      className={
+        props.selectedActivityId === props.id ? "selected" : "activity"
+      }
+      onClick={() => props.selectActivity(props.id)}
+    >
+      <td>{props.startDate}</td>
+      <td>{props.type}</td>
+      <td>{props.name}</td>
+      <td>{props.distance}</td>
+      <td>{props.elapsedTime}</td>
+      <td>{props.averageSpeed}</td>
+      <td>{props.totalElevationGain}</td>
+    </tr>
+  );
+};
+
+export default Activity;
